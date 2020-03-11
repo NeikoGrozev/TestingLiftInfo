e2ef46314d081db0ab2b297bdcbdafb8becb55f5
Neiko7 <neiko_grozev@abv.bg>
2020-03-11 17:25:15 +0200

    Upload CreateAdminProfile...

---
+++ Data/TestingLiftInfo.Data/Seeding/UserSeeder.cs
@@ -0,0 +1,22 @@
+﻿namespace TestingLiftInfo.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Identity;
+
+    internal class UserSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Users.Count() == 1 && !dbContext.UserRoles.Any())
+            {
+                var admin = dbContext.Roles.FirstOrDefault(x => x.Name == "Administrator");
+                var user = dbContext.Users.FirstOrDefault(x => x.Email == "admin@gmail.bg");
+
+                await dbContext.UserRoles.AddAsync(new IdentityUserRole<string> { UserId = user.Id, RoleId = admin.Id });
+            }
+        }
+    }
+}
