b542dec21e8d3827423042333085bd08397c3bad
Neiko7 <neiko_grozev@abv.bg>
2021-01-03 08:27:36 +0200

    Upload mobile menu...

---
+++ Web/TestingLiftInfo.Web/wwwroot/js/mainMenu.js
@@ -0,0 +1,30 @@
+﻿let headerSection = document.querySelector('header section');
+let navigation = document.querySelector('header nav.o-row-container');
+let navigationMobileElement = document.querySelector('header .o-mobile-nav');
+
+let toggleElement = document.querySelector('#toggle');
+
+(function test() {
+    if (window.screen.width <= 900) {
+
+        toggleElement.style.display = 'block'
+    } else {
+        navigation.style.display = 'flex';
+    }
+})()
+
+window.addEventListener('resize', resizeScreen);
+
+function resizeScreen(e) {
+    if (window.screen.width <= 900) {
+        navigation.remove();
+        headerSection.appendChild(toggleElement);
+        toggleElement.style.display = 'block';
+    } else {
+        toggleElement.remove();
+        headerSection.appendChild(navigation);
+        navigation.style.display = 'flex';
+        navigationMobileElement.style.display = 'none';
+        toggleElement.classList.remove('on');
+    }
+}
---
+++ Web/TestingLiftInfo.Web/wwwroot/js/mobileMenu.js
@@ -0,0 +1,53 @@
+﻿let toggle = document.querySelector('#toggle');
+
+toggle.addEventListener('click', clickToggle);
+
+function clickToggle(e) {
+
+    let allValues = e.currentTarget.getAttribute('class');
+
+    if (allValues.includes('on')) {
+        e.currentTarget.classList.remove('on');
+        hideMobileMenu();
+    } else {
+        e.currentTarget.classList.add("on");
+        unhideMobileMenu();
+
+    }
+}
+
+let navigationMobile = document.querySelector('header .o-mobile-nav');
+
+function unhideMobileMenu() {
+    navigationMobile.style.display = 'block';
+}
+
+function hideMobileMenu() {
+    navigationMobile.style.display = 'none';
+}
+
+let mobileLifts = document.querySelector('#mobileLifts');
+let mobileLiftsHiden = document.querySelectorAll('#mobileLiftsHiden');
+mobileLifts.addEventListener('click', hidenLiftsMenu)
+
+function hidenLiftsMenu(e) {
+
+    if (mobileLiftsHiden[0].style.display != 'flex') {
+        mobileLiftsHiden.forEach(x => x.style.display = 'flex');
+    } else {
+        mobileLiftsHiden.forEach(x => x.style.display = 'none');
+    }
+}
+
+let mobileAdministration = document.querySelector('#mobileAdministration');
+let mobileAdministrationHiden = document.querySelectorAll('#mobileAdministrationHiden');
+mobileAdministration.addEventListener('click', hidenAdministrationMenu)
+
+function hidenAdministrationMenu(e) {
+
+    if (mobileAdministrationHiden[0].style.display != 'flex') {
+        mobileAdministrationHiden.forEach(x => x.style.display = 'flex');
+    } else {
+        mobileAdministrationHiden.forEach(x => x.style.display = 'none');
+    }
+}
