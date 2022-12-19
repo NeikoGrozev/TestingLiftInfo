a84104ae91363197e1ecb34c60c250e1f180fa2e
Neiko7 <neiko_grozev@abv.bg>
2022-12-19 08:49:47 +0200

    Inprovement of Create and Detail pages

--- Web/TestingLiftInfo.Web/wwwroot/js/openStreetMap.js
+++ Web/TestingLiftInfo.Web/wwwroot/js/openStreetMap.js
@@ -1,26 +1,34 @@
-﻿const mapOption = {
-    center: [42.697957, 23.322460],
-    zoom: 14
+﻿var map;
+var layer;
+
+function getMapOption() {
+    return mapOption = {
+        center: [42.697957, 23.322460],
+        zoom: 16
+    };
 }
 
-var marker;
+function createMap(mapOptions) {
 
-let map = new L.map('map', mapOption);
+    map = new L.map('map', mapOptions);
 
-let layer = new L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    layer = new L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-L.control.scale({ imperial: false }).addTo(map);
+    L.control.scale({ imperial: false }).addTo(map);
+}
 
-function addMarker(lanLng, address, regNumber, id) {
+function addMarker(lanLng, dragging, address, regNumber, id) {
+    var marker;
 
-    if (lanLng) {
-        marker = new L.marker(lanLng).addTo(map).bindPopup(`<span class="u-text-bold--800">Рег. № <a href="/Administration/Lifts/Detail/${id}">${regNumber}</a></span><br><p class="u-margin--0"><span class="text-bold--800">Адрес: </span>${address}</p>`);
+    if (lanLng && address && regNumber && id) {
+        marker = new L.marker(lanLng).addTo(map).bindPopup(`<span class="u-text-bold--800">Рег. № <a href="/Administration/Lifts/Detail/${id}">${regNumber}</a></span><br><p class="u-margin--0"><span class="u-text-bold--800">Адрес: </span>${address}</p>`);
     } else {
-        marker = new L.marker(mapOption.center).addTo(map);
-        marker.dragging.enable();
+        marker = new L.marker(lanLng).addTo(map);
     }
+
+    dragging ? marker.dragging.enable() : marker.dragging.disable();
 }
 
 function getLatLog(e) {
